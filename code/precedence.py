0f1321aa8a67799b1adff1532c829ff7b534ec70
Connor Robinson <conjames11@gmail.com>
2025-02-06 17:58:13 -0700

    Update precedence.py

--- code/precedence.py
+++ code/precedence.py
@@ -12,20 +12,59 @@ subexpressions in parenthesis, making sure that:
 
 def find_par_exp_forward(expression):
     '''
-    Finds the index of the token within expression correspondin to the end of 
+    Finds the index of the token within expression corresponding to the end of 
     the parenthetical expression in front of a :not: or an :and: operator.
+    Translation: finds matching closing parenthesis ")" after the first "(" it finds
     '''
+
     assert type(expression) == list
     depth = 0
+    for i in range(len(expression)):
+
+        if expression[i] == "(":
+            depth += 1
+
+            # Search for mathing closing ")"
+            for j in range(i+1, len(expression)):
+                if expression[j] == "(":
+                    depth += 1
+                elif expression[j] == ")":
+                    depth -= 1
+
+                    # Check to see if we are back at depth = 0 now which means we found matching ")"
+                    if depth == 0:
+                        return j
+                    
+    print("Error: invalid query expression!")
+    exit(1)
     
 
 def find_par_exp_backward(expression):
     '''
     Finds the index of the token within expression correspondin to the start of 
     the parenthetical expression behind an :and: operator.
+    Translation: starting at the last item, go backwards and find a matching "(" for the first ")" it finds
     '''
     assert type(expression) == list
     depth = 0
+
+    for i in range(len(expression) - 1, -1, -1):
+        if expression[i] == ")":
+            depth += 1
+            
+            # Start at the end and go backwards
+            for j in range(i - 1, -1, -1):
+                if expression[j] == ")":
+                    depth += 1
+                elif expression[j] == "(":
+                    depth -= 1
+
+                    # Check to see if we are back at depth = 0 now which means we found matching "("
+                    if depth == 0:
+                        return j
+                    
+    print("Error: invalid query expression!")
+    exit(1)
 
 def fix_precedence_NOT(expression):
     '''
